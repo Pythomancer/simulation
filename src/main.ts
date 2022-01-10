6d3da5c43dcf63ca08ba9a858355acf4f5524e82
Pythomancer <austermannniklas@outlook.com>
2022-01-10 16:12:46 -0500

    we do a little threejs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import './style.css'
+import * as THREE from 'three';
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
-// var canvas = document.getElementById("simulation")
-// var ctx = canvas.getContext("2d");
-// ctx.fillStyle = "#1f1e33";
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x1f1e33 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+let time = 0.0;
+camera.position.z = 5;
+
+function animate() {
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+  cube.rotation.x = Math.sin(time);
+  cube.rotation.y = Math.cos(time);
+  time+=0.01
+}
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+animate();
